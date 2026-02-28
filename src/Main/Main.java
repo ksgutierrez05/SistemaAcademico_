bc103a9dbd574dd3ce6fd4d618430f4b450464ba
Katherine Sofia Gutierrez Barliza <ksgutierrez@unicesar.edu.co>
2026-02-28 16:04:53 -0500

    feat: crear estructura base de Main con ArrayLists

---
+++ src/Main/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main;
+
+
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Main {
+
+    ArrayList<Object> listaest = new ArrayList<>();
+    ArrayList<Object> listaasig = new ArrayList<>();
+    ArrayList<Object> listanota = new ArrayList<>();
+
+}
