0e1747b6507f4c58ea1e0f5d97c1136d976cebfe
Bayron Enrique Lobo Lopez <belobo@unicesar.edu.co>
2026-02-28 16:33:23 -0500

    feat; agregar contructor y encapsulamiento, implementar contructor, getters, setters

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -29,4 +29,60 @@ class Estudiante {
     private int semestre;
     private int edad;
 
+    public Estudiante(String codigo, String nombre, String apellido, String programa, int semestre, int edad) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.programa = programa;
+        this.semestre = semestre;
+        this.edad = edad;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getPrograma() {
+        return programa;
+    }
+
+    public void setPrograma(String programa) {
+        this.programa = programa;
+    }
+
+    public int getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
 }
