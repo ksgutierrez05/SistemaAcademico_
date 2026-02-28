086dadfccbe2123a43a55ed2f8e4cbd113267685
Katherine Sofia Gutierrez Barliza <ksgutierrez@unicesar.edu.co>
2026-02-28 16:24:21 -0500

    feat: implementar menu principal con todas las opciones

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,7 +4,6 @@
  */
 package Main;
 
-
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
@@ -18,4 +17,37 @@ public class Main {
     ArrayList<Object> listaasig = new ArrayList<>();
     ArrayList<Object> listanota = new ArrayList<>();
 
+    public static void main(String[] args) {
+        mostrarMenu();
+    }
+
+    private static void mostrarMenu() {
+        int op;
+
+        do {
+            op = Integer.parseInt(JOptionPane.showInputDialog(
+                    "====== SISTEMA ACADÉMICO ======\n"
+                    + "1. Gestión Estudiantes\n"
+                    + "2. Gestión Asignaturas\n"
+                    + "3. Gestión Notas\n"
+                    + "4. Salir\n"
+                    + "Elija su opción..."));
+
+            switch (op) {
+                case 1:    //menuEstudiantes();
+                    break;
+                case 2:    //menuAsignaturas();
+                    break;
+                case 3:    //menuNotas();
+                    break;
+                case 4:
+                    JOptionPane.showMessageDialog(null, "Saliendo del programa...");
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opción fuera de rango...");
+            }
+
+        } while (op != 4);
+
+    }
 }
