b2faa48957cc810c0b7204a3beddcbc6a351175c
maria cristina martinez hinojosa <mariacristinamartinez@unicesar.edu.co>
2026-02-28 17:40:03 -0500

    feat: agregar encapsulamiento a clase Nota

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -159,4 +159,61 @@ class Nota{
     private double Nota3;
     private String periodo;
     private double promedio;
+    
+    public void calcularpromedio() {
+        promedio = (double) (Nota1 * 0.3 + Nota2 * 0.3 + Nota3 * 0.4);
+
+    }
+
+    public Nota() {
+    }
+
+    public Nota(double Nota1, double Nota2, double Nota3, String periodo, double promedio) {
+        this.Nota1 = Nota1;
+        this.Nota2 = Nota2;
+        this.Nota3 = Nota3;
+        this.periodo = periodo;
+        this.promedio = promedio;
+    }
+
+    public double getNota1() {
+        return Nota1;
+    }
+
+    public void setNota1(double Nota1) {
+        this.Nota1 = Nota1;
+    }
+
+    public double getNota2() {
+        return Nota2;
+    }
+
+    public void setNota2(double Nota2) {
+        this.Nota2 = Nota2;
+    }
+
+    public double getNota3() {
+        return Nota3;
+    }
+
+    public void setNota3(double Nota3) {
+        this.Nota3 = Nota3;
+    }
+
+    public String getPeriodo() {
+        return periodo;
+    }
+
+    public void setPeriodo(String periodo) {
+        this.periodo = periodo;
+    }
+
+    public double getPromedio() {
+        return promedio;
+    }
+
+    public void setPromedio(double promedio) {
+        this.promedio = promedio;
+    }
+    
 }
