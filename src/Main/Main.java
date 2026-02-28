1a699e38229455b477d2f463518e9d220f33f82a
maria cristina martinez hinojosa <mariacristinamartinez@unicesar.edu.co>
2026-02-28 17:36:10 -0500

    feat: agregar encapsulamiento a clase Asignatura

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -102,5 +102,48 @@ class asignatura{
     private int creditos;
     private String docente;
 
+    public asignatura() {
+    }
+
+    public asignatura(String codigo, String nombre, int creditos, String docente) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.creditos = creditos;
+        this.docente = docente;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCreditos() {
+        return creditos;
+    }
+
+    public void setCreditos(int creditos) {
+        this.creditos = creditos;
+    }
+
+    public String getDocente() {
+        return docente;
+    }
+
+    public void setDocente(String docente) {
+        this.docente = docente;
+    }
+    
+
     
 }
