2b2cf3fad3fff4c9df00baaf9086a4fdff21d968
maria cristina martinez hinojosa <mariacristinamartinez@unicesar.edu.co>
2026-02-28 21:33:24 -0500

    feat: implementar DELETE - eliminar nota

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -123,8 +123,6 @@ public class Main {
 
     }
 
-   
-
     public void BuscarNota() {
         if (listanota.isEmpty()) {
             JOptionPane.showMessageDialog(null,
@@ -152,299 +150,348 @@ public class Main {
         }
 
     }
-    public void ActualizarNota(){
-         if (listanota.isEmpty()) {
+
+    public void ActualizarNota() {
+        if (listanota.isEmpty()) {
             JOptionPane.showMessageDialog(null,
                     "No hay notas registradas.");
             return;
         }
-         
-    String codEst = JOptionPane.showInputDialog(
-            "Ingrese el código del estudiante:");
 
-    String codAsig = JOptionPane.showInputDialog(
-            "Ingrese el código de la asignatura:");
+        String codEst = JOptionPane.showInputDialog(
+                "Ingrese el código del estudiante:");
 
-    String periodo = JOptionPane.showInputDialog(
-            "Ingrese el periodo:");
+        String codAsig = JOptionPane.showInputDialog(
+                "Ingrese el código de la asignatura:");
 
-    for (Nota n : listanota) {
+        String periodo = JOptionPane.showInputDialog(
+                "Ingrese el periodo:");
 
-        if (n.getEstudiante().getCodigo().equalsIgnoreCase(codEst)
-                && n.getAsignatura().getCodigo().equalsIgnoreCase(codAsig)
-                && n.getPeriodo().equalsIgnoreCase(periodo)) {
+        for (Nota n : listanota) {
 
-            JOptionPane.showMessageDialog(null,
-                    "Nota encontrada:\n" + n.toString());
+            if (n.getEstudiante().getCodigo().equalsIgnoreCase(codEst)
+                    && n.getAsignatura().getCodigo().equalsIgnoreCase(codAsig)
+                    && n.getPeriodo().equalsIgnoreCase(periodo)) {
 
-            double n1, n2, n3;
-
-            try {
-
-                n1 = Double.parseDouble(
-                        JOptionPane.showInputDialog("Nueva Nota 1:", n.getNota1()));
-
-                n2 = Double.parseDouble(
-                        JOptionPane.showInputDialog("Nueva Nota 2:", n.getNota2()));
-
-                n3 = Double.parseDouble(
-                        JOptionPane.showInputDialog("Nueva Nota 3:", n.getNota3()));
-
-            } catch (NumberFormatException e) {
                 JOptionPane.showMessageDialog(null,
-                        "Debe ingresar valores numéricos válidos.");
+                        "Nota encontrada:\n" + n.toString());
+
+                double n1, n2, n3;
+
+                try {
+
+                    n1 = Double.parseDouble(
+                            JOptionPane.showInputDialog("Nueva Nota 1:", n.getNota1()));
+
+                    n2 = Double.parseDouble(
+                            JOptionPane.showInputDialog("Nueva Nota 2:", n.getNota2()));
+
+                    n3 = Double.parseDouble(
+                            JOptionPane.showInputDialog("Nueva Nota 3:", n.getNota3()));
+
+                } catch (NumberFormatException e) {
+                    JOptionPane.showMessageDialog(null,
+                            "Debe ingresar valores numéricos válidos.");
+                    return;
+                }
+
+                n.setNota1(n1);
+                n.setNota2(n2);
+                n.setNota3(n3);
+                n.calcularpromedio();
+
+                JOptionPane.showMessageDialog(null,
+                        "Nota actualizada correctamente.\nNuevo promedio: "
+                        + n.getPromedio());
+
                 return;
             }
+        }
 
-            n.setNota1(n1);
-            n.setNota2(n2);
-            n.setNota3(n3);
-            n.calcularpromedio();
+        JOptionPane.showMessageDialog(null,
+                "Nota no encontrada.");
 
+    }
+
+    public void EliminarNota() {
+        if (listanota.isEmpty()) {
             JOptionPane.showMessageDialog(null,
-                    "Nota actualizada correctamente.\nNuevo promedio: "
-                            + n.getPromedio());
-
+                    "No hay notas registradas.");
             return;
         }
+
+        String codEst = JOptionPane.showInputDialog(
+                "Ingrese el código del estudiante :");
+
+        String codAsig = JOptionPane.showInputDialog(
+                "Ingrese el código de la asignatura:");
+
+        String periodo = JOptionPane.showInputDialog(
+                "Ingrese el periodo:");
+        boolean encontrado = false;
+        for (Nota n : listanota) {
+            if (n.getEstudiante().getCodigo().equalsIgnoreCase(codEst)
+                    && n.getAsignatura().getCodigo().equalsIgnoreCase(codAsig)
+                    && n.getPeriodo().equalsIgnoreCase(periodo)) {
+
+                JOptionPane.showMessageDialog(null,
+                        "Nota encontrada:\n" + n.toString());
+
+                int opcion = JOptionPane.showConfirmDialog(
+                        null,
+                        "¿Desea eliminar esta nota?",
+                        "Confirmar eliminación",
+                        JOptionPane.YES_NO_OPTION
+                );
+
+                if (opcion == JOptionPane.YES_OPTION) {
+                    listanota.remove(n);
+                    JOptionPane.showMessageDialog(null,
+                            "Nota eliminada correctamente.");
+                }
+
+                encontrado = true;
+                break;
+            }
+        }
+
+        if (!encontrado) {
+            JOptionPane.showMessageDialog(null,
+                    "Nota no encontrada.");
+        }
+
     }
 
-    JOptionPane.showMessageDialog(null,
-            "Nota no encontrada.");
-        
-    
 }
-    
-    
 
-    class Estudiante {
+class Estudiante {
 
-        private String codigo;
-        private String nombre;
-        private String apellido;
-        private String programa;
-        private int semestre;
-        private int edad;
+    private String codigo;
+    private String nombre;
+    private String apellido;
+    private String programa;
+    private int semestre;
+    private int edad;
 
-        public Estudiante(String codigo, String nombre, String apellido, String programa, int semestre, int edad) {
-            this.codigo = codigo;
-            this.nombre = nombre;
-            this.apellido = apellido;
-            this.programa = programa;
-            this.semestre = semestre;
-            this.edad = edad;
-        }
-
-        public String getCodigo() {
-            return codigo;
-        }
-
-        public void setCodigo(String codigo) {
-            this.codigo = codigo;
-        }
-
-        public String getNombre() {
-            return nombre;
-        }
-
-        public void setNombre(String nombre) {
-            this.nombre = nombre;
-        }
-
-        public String getApellido() {
-            return apellido;
-        }
-
-        public void setApellido(String apellido) {
-            this.apellido = apellido;
-        }
-
-        public String getPrograma() {
-            return programa;
-        }
-
-        public void setPrograma(String programa) {
-            this.programa = programa;
-        }
-
-        public int getSemestre() {
-            return semestre;
-        }
-
-        public void setSemestre(int semestre) {
-            this.semestre = semestre;
-        }
-
-        public int getEdad() {
-            return edad;
-        }
-
-        public void setEdad(int edad) {
-            this.edad = edad;
-        }
-
-        @Override
-        public String toString() {
-            return "Codigo: " + codigo
-                    + ", Nombre: " + nombre
-                    + ", Apellido: " + apellido
-                    + ", Programa: " + programa
-                    + ", Semestre: " + semestre
-                    + ", Edad: " + edad;
-        }
+    public Estudiante(String codigo, String nombre, String apellido, String programa, int semestre, int edad) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.programa = programa;
+        this.semestre = semestre;
+        this.edad = edad;
     }
 
-    class asignatura {
+    public String getCodigo() {
+        return codigo;
+    }
 
-        private String codigo;
-        private String nombre;
-        private int creditos;
-        private String docente;
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
 
-        public asignatura() {
-        }
+    public String getNombre() {
+        return nombre;
+    }
 
-        public asignatura(String codigo, String nombre, int creditos, String docente) {
-            this.codigo = codigo;
-            this.nombre = nombre;
-            this.creditos = creditos;
-            this.docente = docente;
-        }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
-        public String getCodigo() {
-            return codigo;
-        }
+    public String getApellido() {
+        return apellido;
+    }
 
-        public void setCodigo(String codigo) {
-            this.codigo = codigo;
-        }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
 
-        public String getNombre() {
-            return nombre;
-        }
+    public String getPrograma() {
+        return programa;
+    }
 
-        public void setNombre(String nombre) {
-            this.nombre = nombre;
-        }
+    public void setPrograma(String programa) {
+        this.programa = programa;
+    }
 
-        public int getCreditos() {
-            return creditos;
-        }
+    public int getSemestre() {
+        return semestre;
+    }
 
-        public void setCreditos(int creditos) {
-            this.creditos = creditos;
-        }
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
 
-        public String getDocente() {
-            return docente;
-        }
+    public int getEdad() {
+        return edad;
+    }
 
-        public void setDocente(String docente) {
-            this.docente = docente;
-        }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
 
-        @Override
-        public String toString() {
-            return "asignatura{" + "codigo=" + codigo + ", nombre=" + nombre + ", creditos=" + creditos + ", docente=" + docente + '}';
-        }
+    @Override
+    public String toString() {
+        return "Codigo: " + codigo
+                + ", Nombre: " + nombre
+                + ", Apellido: " + apellido
+                + ", Programa: " + programa
+                + ", Semestre: " + semestre
+                + ", Edad: " + edad;
+    }
+}
+
+class asignatura {
+
+    private String codigo;
+    private String nombre;
+    private int creditos;
+    private String docente;
+
+    public asignatura() {
+    }
+
+    public asignatura(String codigo, String nombre, int creditos, String docente) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.creditos = creditos;
+        this.docente = docente;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCreditos() {
+        return creditos;
+    }
+
+    public void setCreditos(int creditos) {
+        this.creditos = creditos;
+    }
+
+    public String getDocente() {
+        return docente;
+    }
+
+    public void setDocente(String docente) {
+        this.docente = docente;
+    }
+
+    @Override
+    public String toString() {
+        return "asignatura{" + "codigo=" + codigo + ", nombre=" + nombre + ", creditos=" + creditos + ", docente=" + docente + '}';
+    }
+
+}
+
+class Nota {
+
+    private Estudiante estudiante;
+    private asignatura Asignatura;
+    private double Nota1;
+    private double Nota2;
+    private double Nota3;
+    private String periodo;
+    private double promedio;
+
+    public void calcularpromedio() {
+        promedio = (double) (Nota1 * 0.3 + Nota2 * 0.3 + Nota3 * 0.4);
 
     }
 
-    class Nota {
-
-        private Estudiante estudiante;
-        private asignatura Asignatura;
-        private double Nota1;
-        private double Nota2;
-        private double Nota3;
-        private String periodo;
-        private double promedio;
-
-        public void calcularpromedio() {
-            promedio = (double) (Nota1 * 0.3 + Nota2 * 0.3 + Nota3 * 0.4);
-
-        }
-
-        public Nota() {
-        }
-
-        public Nota(Estudiante estudiante, asignatura Asignatura, double Nota1, double Nota2, double Nota3, String periodo, double promedio) {
-            this.estudiante = estudiante;
-            this.Asignatura = Asignatura;
-            this.Nota1 = Nota1;
-            this.Nota2 = Nota2;
-            this.Nota3 = Nota3;
-            this.periodo = periodo;
-            this.promedio = promedio;
-        }
-
-        public asignatura getAsignatura() {
-            return Asignatura;
-        }
-
-        public void setAsignatura(asignatura Asignatura) {
-            this.Asignatura = Asignatura;
-        }
-
-        public Estudiante getEstudiante() {
-            return estudiante;
-        }
-
-        public void setEstudiante(Estudiante estudiante) {
-            this.estudiante = estudiante;
-        }
-
-        public double getNota1() {
-            return Nota1;
-        }
-
-        public void setNota1(double Nota1) {
-            this.Nota1 = Nota1;
-        }
-
-        public double getNota2() {
-            return Nota2;
-        }
-
-        public void setNota2(double Nota2) {
-            this.Nota2 = Nota2;
-        }
-
-        public double getNota3() {
-            return Nota3;
-        }
-
-        public void setNota3(double Nota3) {
-            this.Nota3 = Nota3;
-        }
-
-        public String getPeriodo() {
-            return periodo;
-        }
-
-        public void setPeriodo(String periodo) {
-            this.periodo = periodo;
-        }
-
-        public double getPromedio() {
-            return promedio;
-        }
-
-        public void setPromedio(double promedio) {
-            this.promedio = promedio;
-        }
-
-        @Override
-        public String toString() {
-            return "Nota{" + "estudiante=" + estudiante
-                    + ", Asignatura=" + Asignatura
-                    + ", Nota1=" + Nota1
-                    + ", Nota2=" + Nota2
-                    + ", Nota3=" + Nota3
-                    + ", periodo=" + periodo
-                    + ", promedio=" + promedio + '}';
-        }
-
+    public Nota() {
     }
+
+    public Nota(Estudiante estudiante, asignatura Asignatura, double Nota1, double Nota2, double Nota3, String periodo, double promedio) {
+        this.estudiante = estudiante;
+        this.Asignatura = Asignatura;
+        this.Nota1 = Nota1;
+        this.Nota2 = Nota2;
+        this.Nota3 = Nota3;
+        this.periodo = periodo;
+        this.promedio = promedio;
+    }
+
+    public asignatura getAsignatura() {
+        return Asignatura;
+    }
+
+    public void setAsignatura(asignatura Asignatura) {
+        this.Asignatura = Asignatura;
+    }
+
+    public Estudiante getEstudiante() {
+        return estudiante;
+    }
+
+    public void setEstudiante(Estudiante estudiante) {
+        this.estudiante = estudiante;
+    }
+
+    public double getNota1() {
+        return Nota1;
+    }
+
+    public void setNota1(double Nota1) {
+        this.Nota1 = Nota1;
+    }
+
+    public double getNota2() {
+        return Nota2;
+    }
+
+    public void setNota2(double Nota2) {
+        this.Nota2 = Nota2;
+    }
+
+    public double getNota3() {
+        return Nota3;
+    }
+
+    public void setNota3(double Nota3) {
+        this.Nota3 = Nota3;
+    }
+
+    public String getPeriodo() {
+        return periodo;
+    }
+
+    public void setPeriodo(String periodo) {
+        this.periodo = periodo;
+    }
+
+    public double getPromedio() {
+        return promedio;
+    }
+
+    public void setPromedio(double promedio) {
+        this.promedio = promedio;
+    }
+
+    @Override
+    public String toString() {
+        return "Nota{" + "estudiante=" + estudiante
+                + ", Asignatura=" + Asignatura
+                + ", Nota1=" + Nota1
+                + ", Nota2=" + Nota2
+                + ", Nota3=" + Nota3
+                + ", periodo=" + periodo
+                + ", promedio=" + promedio + '}';
+    }
+
+}
 
 }
